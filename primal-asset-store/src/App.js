7bf1a83eb2c7fa1aced9816fac1f895e3ada0fcf
TabishMomin <mominmota19ie@student.mes.ac.in>
2021-08-25 19:17:29 +0530

    adding react module

--- primal-asset-store/src/App.js
+++ primal-asset-store/src/App.js
@@ -1,11 +1,43 @@
-import Login from "./components/Login";
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import UI from './Pages/Ui';
+import Game from './Pages/Game';
+import About from './Pages/About';
+import FAQ from './Pages/Faq';
+import Login from './components/Login';
+import SearchBar from './Pages/SearchBar';
 
-const App = () => {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
-};
+function App() {
+return (
+	<Router>
+	<div>
+	<Navbar />
+	<Switch>
+		<Route path='/' exact component={Home} />
+		<Route path='/ui' component={UI} />
+		<Route path='/game' component={Game} />
+		<Route path='/about' component={About} />
+		<Route path='/faq' component={FAQ} />
+		<Route path='/components/Login' component={Login} />
+	</Switch>
+	<Navbar />
+	</div>
+
+	<div>
+	<Switch>
+		<Route path='/SearchBar' component={SearchBar} />
+	</Switch>
+	</div>
+	</Router>
+	
+);
+}
 
 export default App;
+
+
+
+

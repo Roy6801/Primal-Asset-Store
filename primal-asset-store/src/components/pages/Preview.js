8eb039229f9b2bbe13e9421afe1578ae25fdf80b
Roy6801 <>
2021-09-07 22:28:44 +0530

    master resolved

--- primal-asset-store/src/components/pages/Preview.js
+++ primal-asset-store/src/components/pages/Preview.js
@@ -1,44 +1,29 @@
-import React from 'react';
+import PropTypes from "prop-types";
 
-function Preview(props) {
-  let {
-    Preview
-  } = props;
-
+const Preview = ({ assetInfo }) => {
   return (
+    <div className="">
       <div className="">
-        <div className="">
-          <a href="#">
-            <img
-              alt={Preview.name}
-              className=""
-              src={Preview.picture} />
-          </a>
-        </div>
-
-        <h4
-          className=""
-          title={Preview.name}>
-          <a href="#">
-            {Preview.name}
-          </a>
-        </h4>
-
-        <h5
-          className=""
-          title={Preview.brand_name}>
-          {`by ${Preview.brand_name}`}
-        </h5>
-
-        <div className="">
-          {`${Preview.price}$`}
-        </div>
+        <a href="#">
+          <img alt={assetInfo.name} className="" src={assetInfo.picture} />
+        </a>
       </div>
+
+      <h4 className="" title={assetInfo.name}>
+        <a href="#">{assetInfo.name}</a>
+      </h4>
+
+      <h5 className="" title={assetInfo.brand_name}>
+        {`by ${assetInfo.brand_name}`}
+      </h5>
+
+      <div className="">{`${assetInfo.price}$`}</div>
+    </div>
   );
-}
+};
 
 Preview.propTypes = {
-  Preview: React.PropTypes.object.isRequired
+  Preview: PropTypes.object.isRequired,
 };
 
 export default Preview;

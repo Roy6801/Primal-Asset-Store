abcbe3ed7e8f69b928058ca63b5dac155990ca68
RohitRana19 <rohitrana0019@gmail.com>
2021-09-08 19:49:59 +0530

    database created and register function created

--- api/primal_user/admin.py
+++ api/primal_user/admin.py
@@ -3,3 +3,9 @@ from .models import *
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Plans)
+admin.site.register(Tags)
+admin.site.register(Asset)
+admin.site.register(Review)
+admin.site.register(SearchHistory)
+admin.site.register(Order)
+
--- api/primal_user/models.py
+++ api/primal_user/models.py
@@ -1,10 +1,10 @@
 from re import T
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core.validators import BaseValidator, RegexValidator
 
 # Create your models here.
 class User(models.Model):
-    useranme =models.CharField(max_length = 20, primary_key= True)
+    userName =models.CharField(max_length = 20, primary_key= True)
     firstName = models.CharField(max_length = 20, null=True)
     lastName = models.CharField(max_length= 20,null=True)
     phoneNumberRegex = RegexValidator(regex = r"^\+?1?\d{8,15}$")
@@ -16,6 +16,53 @@ class User(models.Model):
     securityPin = models.IntegerField( null=True)
     accountCreationDate = models.DateTimeField()
 
+    def __str__(self):
+        return self.userName
+
 class Plans(models.Model):
     planId = models.CharField(max_length=10, primary_key=True,default="DEFAULT VALUE")
     quota = models.CharField(max_length=10)
+
+class Tags(models.Model):
+    keywordId = models.CharField(max_length=20, primary_key=True)
+    keyword = models.CharField(max_length=20)
+
+class SearchHistory(models.Model):
+    historyId = models.CharField(max_length=20, primary_key=True)
+    userId = models.ForeignKey('primal_user.User',on_delete=models.CASCADE)
+    keywordId = models.ForeignKey('primal_user.Tags', on_delete=models.CASCADE)
+
+class Asset(models.Model):
+    assetId = models.CharField(max_length=20,primary_key=True)
+    devUserId = models.ForeignKey('primal_user.User',on_delete=models.CASCADE)
+    keywordId = models.ForeignKey('primal_user.Tags',on_delete=models.CASCADE)
+    assetName = models.CharField(max_length=50,null=False)
+    description= models.TextField(blank=True)
+    features = models.TextField(blank=True)
+    uploadedDate = models.DateField(auto_now_add=True)
+    typeId = models.BooleanField(default=True)
+    paidStatus = models.BooleanField(default=False)
+    price = models.IntegerField(null=True)
+    size= models.FloatField(null=False)
+    downloadCount = models.BigIntegerField(null=True)
+    version = models.CharField(max_length=10)
+
+class Review(models.Model):
+    reviewId= models.CharField(max_length=20,primary_key=True)
+    userId = models.ForeignKey('primal_user.User',on_delete=models.CASCADE)
+    assetId = models.ForeignKey('primal_user.Asset',on_delete=models.CASCADE)
+    rating = models.FloatField(null=True)
+    comment = models.TextField(blank=True)
+    date = models.DateField(auto_now_add=True)
+
+class Order(models.Model):
+    cartId = models.CharField(max_length=20, primary_key=True)
+    userId = models.ForeignKey('primal_user.User',on_delete=models.CASCADE)
+    assetId = models.ForeignKey('primal_user.Asset',on_delete=models.CASCADE)
+    orderId = models.CharField(max_length=20)
+    transactionId = models.CharField(max_length=50)
+    success = models.BooleanField(default=False)
+    purchaseDate = models.DateField(auto_now_add=True)
+
+
+    

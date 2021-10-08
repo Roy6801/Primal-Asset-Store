86d8ef7f9d8400635ab65afb158d155cf13a0496
Roy6801 <>
2021-10-08 18:37:29 +0530

    Edit Asset

--- primal-asset-store/src/components/elements/AssetsList.js
+++ primal-asset-store/src/components/elements/AssetsList.js
@@ -1,9 +1,36 @@
-const AssetsList = () => {
-  return (
-    <div>
-      <h1>AssetsList</h1>
-    </div>
-  );
+import { useState } from "react";
+import service from "../functions/service";
+import AdminAsset from "./AdminAsset";
+
+const AssetsList = ({ devId }) => {
+  const [aList, setAList] = useState();
+
+  if (!aList) {
+    service
+      .assetList(devId)
+      .then((resp) => {
+        setAList(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  if (devId && aList) {
+    return (
+      <div>
+        {aList.map((assetInfo, index) => {
+          return (
+            <div style={{ backgroundColor: "yellow" }} key={index}>
+              <AdminAsset googleId={devId} props={assetInfo} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default AssetsList;

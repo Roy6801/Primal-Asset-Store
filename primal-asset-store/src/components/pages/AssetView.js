a57bd8e88fa58ef2db4e6d323441447f01fa5d85
“Rohit <rohitrana0019@gmail.com>
2021-10-13 20:31:35 +0530

    review page css

--- primal-asset-store/src/components/pages/AssetView.js
+++ primal-asset-store/src/components/pages/AssetView.js
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import service from "../functions/service";
 import NotFound from "../NotFound";
 import Review from "../elements/Review";
+import "../stylesheets/AssetReview.css";
 
 const AssetView = (props) => {
   const assetId = props.match.params.assetId;
@@ -34,54 +35,77 @@ const AssetView = (props) => {
       return <NotFound />;
     } else {
       return (
-        <div style={{ backgroundColor: "teal" }}>
-          <NavLink to={`/user/view/asset/${assetInfo.assetId}`}>
-            {assetInfo.assetName}
-          </NavLink>
-          <div>
-            <label>Publisher</label>
-            <NavLink to={`/user/view/publisher/${publisherInfo.userName}`}>
-              {publisherInfo.userName}
-            </NavLink>
+        <div className="Asset-review">
+          <div className="asset-container">
+            <div className="image-container-review"></div>
+            <div className="review-info">
+              <NavLink to={`/user/view/asset/${assetInfo.assetId}`}>
+                {assetInfo.assetName}
+              </NavLink>
+            </div>
+            <div className="review-info">
+              <div className="info">
+                <label className="label-review">Publisher</label>
+                <NavLink to={`/user/view/publisher/${publisherInfo.userName}`}>
+                  {publisherInfo.userName}
+                </NavLink>
+              </div>
+              <div className="info1"></div>
+            </div>
+            <div className="review-info">
+              <label className="label-border">
+                {assetInfo.typeId ? "Game Asset" : "UI Asset"}
+              </label>
+            </div>
+            <div className="review-info">
+              <label>{`Version: ${assetInfo.version}`}</label>
+            </div>
+            <div className="review-info">
+              <label>{`File Size: ${
+                assetInfo.size > 1024 * 1024
+                  ? assetInfo.size > 1024 * 1024 * 1024
+                    ? `${Math.round(
+                        parseFloat(assetInfo.size) / (1024 * 1024 * 1024),
+                        2
+                      )} GB`
+                    : `${Math.round(
+                        parseFloat(assetInfo.size) / (1024 * 1024),
+                        2
+                      )} MB`
+                  : `${Math.round(parseFloat(assetInfo.size) / 1024, 2)} KB`
+              }`}</label>
+            </div>
+            <div className="review-info">
+              <label>Description :</label>
+              <p>{assetInfo.description}</p>
+            </div>
+            <div className="review-info">
+              <label>Features :</label>
+              <p>{assetInfo.features}</p>
+            </div>
+            <div className="review-info">
+              <label>{assetInfo.createdDate}</label>
+            </div>
+            <div className="review-info">
+              <label>
+                {assetInfo.price
+                  ? `${assetInfo.price} ${assetInfo.currency}`
+                  : "FREE"}
+              </label>
+            </div>
+            <div className="review-info">
+              <label>
+                {`Downloaded ${
+                  assetInfo.downloadCount ? assetInfo.downloadCount : 0
+                } times`}
+              </label>
+            </div>
+
+            <button className="btn-use3">Add to Cart</button>
           </div>
-          <label>{assetInfo.typeId ? "Game Asset" : "UI Asset"}</label>
-          <label>{`Version: ${assetInfo.version}`}</label>
-          <label>{`File Size: ${
-            assetInfo.size > 1024 * 1024
-              ? assetInfo.size > 1024 * 1024 * 1024
-                ? `${Math.round(
-                    parseFloat(assetInfo.size) / (1024 * 1024 * 1024),
-                    2
-                  )} GB`
-                : `${Math.round(
-                    parseFloat(assetInfo.size) / (1024 * 1024),
-                    2
-                  )} MB`
-              : `${Math.round(parseFloat(assetInfo.size) / 1024, 2)} KB`
-          }`}</label>
           <div>
-            <label>Description</label>
-            <p>{assetInfo.description}</p>
+            <Review assetInfo={assetInfo} />
           </div>
-          <div>
-            <label>Features</label>
-            <p>{assetInfo.features}</p>
-          </div>
-          <label>{assetInfo.createdDate}</label>
-          <label>
-            {assetInfo.price
-              ? `${assetInfo.price} ${assetInfo.currency}`
-              : "FREE"}
-          </label>
-          <div>
-            <label>
-              {`Downloaded ${
-                assetInfo.downloadCount ? assetInfo.downloadCount : 0
-              } times`}
-            </label>
-          </div>
-          <button>Add to Cart</button>
-          <Review assetInfo={assetInfo} />
         </div>
       );
     }
